2cd1d24d67495f4b12f5ec48aa0d1749aa3230e8
Akatsuki-Misaki <i@ovofish.com>
2025-07-04 21:37:23 +0800

    Update:测试版本

---
+++ data.py
@@ -0,0 +1,13 @@
+img_data = {
+    "loadbg": "",
+    "vol1": "",
+    "vol2": "",
+    "vol3": "",
+    "vol4": "",
+    "after": "",
+    "Mainbg": "",
+    "menubg": "",
+    "start_install_btn": "",
+    "exit_btn": "",
+    "icon": "",
+}
